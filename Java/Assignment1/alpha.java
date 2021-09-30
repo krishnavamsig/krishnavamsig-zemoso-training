62886d390b3791972f872047c85134a41e74cb6d
Krishnavamsi <Krishnavamsi.gujju@gmail.com>
2021-09-30 08:56:22 +0530

    Assignment2

---
+++ Java/Assignment1/alpha.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+public class alpha{
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+      System.out.print("\nEnter your String ");
+    String str;
+    str=in.nextLine();
+    str=str.toLowerCase();
+        boolean[] alphaList = new boolean[26];
+        int index = 0;
+        int flag = 1;
+        for (int i = 0; i < str.length(); i++) {
+            if( str.charAt(i) >= 'a' &&  str.charAt(i) <= 'z') {
+                index = str.charAt(i) - 'a';
+            }
+            alphaList[index] = true;
+        }
+        for (int i = 0; i <= 25; i++) {
+            if (alphaList[i] == false)
+                flag = 0;
+        }
+        System.out.print("String: " + str);
+        if (flag == 1)
+            System.out.print("\nThe input string contains all the letters of the alphabet a-z.\n ");
+        else
+            System.out.print("\nThe input string does not contains all the letters of the alphabet a-z\n ");
+    }
+}
