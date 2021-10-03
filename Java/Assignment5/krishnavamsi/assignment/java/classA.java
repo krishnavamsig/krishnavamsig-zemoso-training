30aabc662c53db40a70dd807303ff7751d17098f
krishnavamsig <krishnavamsi.gujju@gmail.com>
2021-10-03 22:34:14 +0530

    Assignment5

---
+++ Java/Assignment5/krishnavamsi/assignment/java/classA.java
@@ -0,0 +1,15 @@
+package krishnavamsi.assignment.java;
+public class classA {
+    int n;
+    char ch;
+    public void display(){
+        System.out.println(n);
+		System.out.println(ch);
+    }
+    public void show(){
+		int a,b;
+		// System.out.println("The value of c and d are "+ca;
+		// System.out.println(b);
+        //The local variable a,b are not initialized so cannot be printed
+		}
+}
